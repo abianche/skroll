1581c6eff3afecab65529ad198a7da07adbb94c3
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-09-20 18:00:27 +0200

    refactor: moving to electron

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,7 +26,7 @@
  * ```
  */
 
-import './index.css';
-import './App.tsx';
+import "./index.css";
+import "./App.tsx";
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
