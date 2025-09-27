6d335b05aad6872290fc759b8e3e2c62b3b6424e
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-09-28 00:04:10 +0200

    feat(parser): scaffold tree-sitter runtime parser

--- packages/tree-sitter-skroll/src/index.ts
+++ packages/tree-sitter-skroll/src/index.ts
@@ -1,2 +1,24 @@
-// Placeholder entrypoint for the Skroll DSL grammar package.
-export {};
+import Parser from "tree-sitter";
+
+let cachedLanguage: Parser.Language | null = null;
+
+function loadLanguage(): Parser.Language {
+  if (!cachedLanguage) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const binding = require("../bindings/node") as { language: Parser.Language };
+    cachedLanguage = binding.language;
+  }
+  return cachedLanguage;
+}
+
+export function getLanguage(): Parser.Language {
+  return loadLanguage();
+}
+
+export function createParser(): Parser {
+  const parser = new Parser();
+  parser.setLanguage(loadLanguage());
+  return parser;
+}
+
+export type { SyntaxNode } from "tree-sitter";
